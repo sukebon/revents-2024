b970fbb3c6d7678baff4273c63e23bcce6fb8d9e
sukebon <39957486+sukebon@users.noreply.github.com>
2024-03-20 09:42:30 +0900

    end of section 1

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,10 @@
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+
+function App() {
+  return (
+    <h1>Welcome to Revents</h1>
+  );
+}
+
+export default App;
